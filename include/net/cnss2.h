4eaa2c56bf81a9b333bcfd1825f194622548967a
Yue Ma <yuem@codeaurora.org>
2020-02-07 12:02:12 -0800

    cnss2: Expose APIs to prevent and allow PCIe L1 state
    WLAN driver can use the APIs to control if PCIe can enter L1 and
    L1ss states.
    
    Change-Id: Iad784b1ca66199ad61c471635a67542afd34480e
    Signed-off-by: Yue Ma <yuem@codeaurora.org>

--- include/net/cnss2.h
+++ include/net/cnss2.h
@@ -1,5 +1,15 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved. */
+/* Copyright (c) 2016-2019, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #ifndef _NET_CNSS2_H
 #define _NET_CNSS2_H
@@ -158,6 +168,8 @@ enum cnss_recovery_reason {
 extern int cnss_wlan_register_driver(struct cnss_wlan_driver *driver);
 extern void cnss_wlan_unregister_driver(struct cnss_wlan_driver *driver);
 extern void cnss_device_crashed(struct device *dev);
+extern int cnss_pci_prevent_l1(struct device *dev);
+extern void cnss_pci_allow_l1(struct device *dev);
 extern int cnss_pci_link_down(struct device *dev);
 extern int cnss_pci_is_device_down(struct device *dev);
 extern void cnss_schedule_recovery(struct device *dev,
